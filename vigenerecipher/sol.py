205c7c05e344b7eb773838c7e3918147c8140550
Patrick Gebhardt <patrick.gebhardt@gmail.com>
2021-01-03 23:27:34 +0100

    solved vigenere-cipher

---
+++ vigenerecipher/sol.py
@@ -0,0 +1,29 @@
+import string
+import numpy
+
+input = "gwox{RgqssihYspOntqpxs}"
+key = "blorpy"
+
+alphabet = list(string.ascii_lowercase)
+
+fail = 0
+output = ""
+
+for i in range(0, len(input)):
+    if(input[i] in alphabet):
+        c = key[(i - fail) % len(key)]
+        print(c)
+        keyIndex = alphabet.index(c)
+        msgIndex = alphabet.index(input[i])
+        output += alphabet[(msgIndex - keyIndex + len(alphabet)) % 26];
+    elif(input[i].lower() in alphabet):
+        c = key[(i - fail) % len(key)].lower()
+        print(c)
+        keyIndex = alphabet.index(c)
+        msgIndex = alphabet.index(input[i].lower())
+        output += alphabet[(msgIndex - keyIndex + len(alphabet)) % 26].upper();
+    else:
+        output += input[i]
+        fail+=1
+
+print(output)
